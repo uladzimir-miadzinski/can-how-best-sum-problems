28e52f75b6402ca35c63106d4fb24474bab532ec
uladzimir-miadzinski <webmedinsky@gmail.com>
2023-07-24 14:16:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# can-how-best-sum-problems
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-sbgns2)
---
+++ index.js
@@ -0,0 +1,9 @@
+function canSum(target, numbers) {
+
+}
+
+canSum(7, [2, 3]);
+canSum(7, [5, 3, 4, 7]);
+canSum(7, [2, 4]);
+canSum(8, [2, 3, 5]);
+canSum(300, [7, 14]);
